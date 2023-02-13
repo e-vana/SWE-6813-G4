63f9ed7b7f75e5bc757a2defa5a5d6f4d00ecc0b
jcrab421 <103462168+jcrab421@users.noreply.github.com>
2023-02-12 20:28:06 -0500

    John Test Commit

--- application/server/routes/users.routes.ts
+++ application/server/routes/users.routes.ts
@@ -5,6 +5,7 @@ import { dbConnectionString } from "../config/database.config";
 
 const router: Router = Router();
 
+
 router.get("/", async (req: Request, res: Response): Promise<void> => {
   try {
     let connection = await mysql.createConnection(dbConnectionString!);
