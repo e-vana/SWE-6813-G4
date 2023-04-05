3cc8bb52b532b253ef3331588a2d7c1015c07ab3
Evan <evjava@gmail.com>
2023-04-04 21:36:01 -0400

    Update matchmaking.test.ts

--- application/server/tests/matchmaking.test.ts
+++ application/server/tests/matchmaking.test.ts
@@ -2,21 +2,23 @@ import { MatchMakingEntry, matchmaking } from "../utility/matchmaking";
 import data from '../data/matchmaking_sample_data.json';
 import data_small from '../data/matchmaking_sample_data_small.json';
 import data_one from '../data/matchmaking_sample_data_one.json';
+//dataset needs to be regenerated to include the active: {INTEGER} key
 const testData = data as MatchMakingEntry[];
 const testDataSmall = data_small as MatchMakingEntry[];
 const testDataOne = data_one as MatchMakingEntry[];
 
 describe('Matchmaking Function test suite', ()=> {
-  it('Returns null if there are no players in the queue.', () => {
+  it('If passed an empty array, return NULL', () => {
     expect(matchmaking([])).toBeNull;
   })
-  it('If game player count < 5 return null ', () => {
+  it('If passed an array of players and there are no games with lobbies of size 5, return NULL', () => {
     expect(matchmaking(testDataSmall)).toBeNull;
   })
-  it('If game player count > 5, create lobbies for database insertion', () => {
+  it('If a game with player count > 5, create lobbies for database insertion', () => {
     expect(matchmaking(testData)).toBeTruthy;
   })
   it('Checks small dataset for verifiable result', ()=> {
+    //should probably deep compare known good data
     let t = matchmaking(testDataOne);
     expect(t![0].game_id).toEqual(5);
     expect(t![0].lobby.length).toEqual(5);
