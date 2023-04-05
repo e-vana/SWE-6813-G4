d70a516ab39c9731e05147b772f0de0a23d516c6
Evan <evjava@gmail.com>
2023-04-05 12:16:07 -0400

    changed language for test clarity

--- application/server/tests/matchmakingJob.test.ts
+++ application/server/tests/matchmakingJob.test.ts
@@ -1,11 +1,18 @@
 import { matchmakingJob } from "../utility/matchmakingJob";
 import { createLobbyAndInsertPlayersIntoLobby } from "../utility/createLobbyAndInsertPlayersIntoLobby";
-import { MatchMakingEntryRowPacket, getAllUsersInMatchmakingQueue } from "../utility/getAllUsersInMatchmakingQueue";
-import { MatchMakingEntry, MatchMakingLobby, matchmaking } from "../utility/matchmaking";
+import {
+  MatchMakingEntryRowPacket,
+  getAllUsersInMatchmakingQueue,
+} from "../utility/getAllUsersInMatchmakingQueue";
+import {
+  MatchMakingEntry,
+  MatchMakingLobby,
+  matchmaking,
+} from "../utility/matchmaking";
 
-jest.mock("../utility/createLobbyAndInsertPlayersIntoLobby")
-jest.mock("../utility/getAllUsersInMatchmakingQueue")
-jest.mock("../utility/matchmaking")
+jest.mock("../utility/createLobbyAndInsertPlayersIntoLobby");
+jest.mock("../utility/getAllUsersInMatchmakingQueue");
+jest.mock("../utility/matchmaking");
 
 const mockGetAllUsers: MatchMakingEntry[] = [
   {
@@ -15,7 +22,7 @@ const mockGetAllUsers: MatchMakingEntry[] = [
     vibe_weight: 3,
     content_weight: 3,
     time_entered: "2008-11-11 13:23:44",
-    active: 1
+    active: 1,
   },
   {
     session_id: 2,
@@ -24,7 +31,7 @@ const mockGetAllUsers: MatchMakingEntry[] = [
     vibe_weight: 3,
     content_weight: 3,
     time_entered: "2008-11-11 13:23:44",
-    active: 1
+    active: 1,
   },
   {
     session_id: 3,
@@ -33,7 +40,7 @@ const mockGetAllUsers: MatchMakingEntry[] = [
     vibe_weight: 3,
     content_weight: 3,
     time_entered: "2008-11-11 13:23:44",
-    active: 1
+    active: 1,
   },
   {
     session_id: 4,
@@ -42,7 +49,7 @@ const mockGetAllUsers: MatchMakingEntry[] = [
     vibe_weight: 3,
     content_weight: 3,
     time_entered: "2008-11-11 13:23:44",
-    active: 1
+    active: 1,
   },
   {
     session_id: 5,
@@ -51,49 +58,84 @@ const mockGetAllUsers: MatchMakingEntry[] = [
     vibe_weight: 3,
     content_weight: 3,
     time_entered: "2008-11-11 13:23:44",
-    active: 1
-  }
-]
-const mockMatchmakingData: MatchMakingLobby[] = [{game_id: 1, lobby: mockGetAllUsers}] 
-describe("matchmakingJob", () => {
+    active: 1,
+  },
+];
+const mockMatchmakingData: MatchMakingLobby[] = [
+  { game_id: 1, lobby: mockGetAllUsers },
+];
+describe("Test the function matchmakingJob for desired behavior.", () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
   it("should not call any functions if there are less than 5 users in the matchmaking queue", async () => {
-    const mockedGetAllUsersInMatchmakingQueue = getAllUsersInMatchmakingQueue as jest.MockedFunction<typeof getAllUsersInMatchmakingQueue>;
-    const mockedCreateLobbyAndInsertPlayersIntoLobby = createLobbyAndInsertPlayersIntoLobby as jest.MockedFunction<typeof createLobbyAndInsertPlayersIntoLobby>;
+    const mockedGetAllUsersInMatchmakingQueue =
+      getAllUsersInMatchmakingQueue as jest.MockedFunction<
+        typeof getAllUsersInMatchmakingQueue
+      >;
+    const mockedCreateLobbyAndInsertPlayersIntoLobby =
+      createLobbyAndInsertPlayersIntoLobby as jest.MockedFunction<
+        typeof createLobbyAndInsertPlayersIntoLobby
+      >;
     mockedGetAllUsersInMatchmakingQueue.mockResolvedValueOnce([]);
     await matchmakingJob();
     expect(mockedGetAllUsersInMatchmakingQueue).toHaveBeenCalled();
     expect(mockedCreateLobbyAndInsertPlayersIntoLobby).not.toHaveBeenCalled();
   });
   it("should call the matchmaking function if there are at least 5 players in the matchmaking queue.", async () => {
-    const mockedGetAllUsersInMatchmakingQueue = getAllUsersInMatchmakingQueue as jest.MockedFunction<typeof getAllUsersInMatchmakingQueue>;
-    const mockMatchmaking = matchmaking as jest.MockedFunction<typeof matchmaking>;
-    mockedGetAllUsersInMatchmakingQueue.mockResolvedValueOnce(mockGetAllUsers as MatchMakingEntryRowPacket[]);
-    mockMatchmaking.mockReturnValueOnce(mockMatchmakingData)
+    const mockedGetAllUsersInMatchmakingQueue =
+      getAllUsersInMatchmakingQueue as jest.MockedFunction<
+        typeof getAllUsersInMatchmakingQueue
+      >;
+    const mockMatchmaking = matchmaking as jest.MockedFunction<
+      typeof matchmaking
+    >;
+    mockedGetAllUsersInMatchmakingQueue.mockResolvedValueOnce(
+      mockGetAllUsers as MatchMakingEntryRowPacket[]
+    );
+    mockMatchmaking.mockReturnValueOnce(mockMatchmakingData);
     await matchmakingJob();
     expect(mockedGetAllUsersInMatchmakingQueue).toHaveBeenCalled();
     expect(mockMatchmaking).toHaveBeenCalled();
   });
-  it("should not call the createLobbyAndInsertPlayersIntoLobby function if matchmaking returns NULL", async () => {
-    const mockedGetAllUsersInMatchmakingQueue = getAllUsersInMatchmakingQueue as jest.MockedFunction<typeof getAllUsersInMatchmakingQueue>;
-    const mockMatchmaking = matchmaking as jest.MockedFunction<typeof matchmaking>;
-    const mockedCreateLobbyAndInsertPlayersIntoLobby = createLobbyAndInsertPlayersIntoLobby as jest.MockedFunction<typeof createLobbyAndInsertPlayersIntoLobby>;
-    mockedGetAllUsersInMatchmakingQueue.mockResolvedValueOnce(mockGetAllUsers as MatchMakingEntryRowPacket[]);
-    mockMatchmaking.mockReturnValueOnce(null)
+  it("should not call the createLobbyAndInsertPlayersIntoLobby function if matchmaking function returns NULL", async () => {
+    const mockedGetAllUsersInMatchmakingQueue =
+      getAllUsersInMatchmakingQueue as jest.MockedFunction<
+        typeof getAllUsersInMatchmakingQueue
+      >;
+    const mockMatchmaking = matchmaking as jest.MockedFunction<
+      typeof matchmaking
+    >;
+    const mockedCreateLobbyAndInsertPlayersIntoLobby =
+      createLobbyAndInsertPlayersIntoLobby as jest.MockedFunction<
+        typeof createLobbyAndInsertPlayersIntoLobby
+      >;
+    mockedGetAllUsersInMatchmakingQueue.mockResolvedValueOnce(
+      mockGetAllUsers as MatchMakingEntryRowPacket[]
+    );
+    mockMatchmaking.mockReturnValueOnce(null);
     await matchmakingJob();
     expect(mockedGetAllUsersInMatchmakingQueue).toHaveBeenCalled();
     expect(mockMatchmaking).toHaveBeenCalled();
     expect(mockedCreateLobbyAndInsertPlayersIntoLobby).not.toHaveBeenCalled();
   });
-  it("should call the createLobbyAndInsertPlayersIntoLobby function if matchmaking returns a value", async () => {
-    const mockedGetAllUsersInMatchmakingQueue = getAllUsersInMatchmakingQueue as jest.MockedFunction<typeof getAllUsersInMatchmakingQueue>;
-    const mockMatchmaking = matchmaking as jest.MockedFunction<typeof matchmaking>;
-    const mockedCreateLobbyAndInsertPlayersIntoLobby = createLobbyAndInsertPlayersIntoLobby as jest.MockedFunction<typeof createLobbyAndInsertPlayersIntoLobby>;
-    mockedGetAllUsersInMatchmakingQueue.mockResolvedValueOnce(mockGetAllUsers as MatchMakingEntryRowPacket[]);
-    mockMatchmaking.mockReturnValueOnce(mockMatchmakingData)
+  it("should call the createLobbyAndInsertPlayersIntoLobby function if matchmaking function returns a value", async () => {
+    const mockedGetAllUsersInMatchmakingQueue =
+      getAllUsersInMatchmakingQueue as jest.MockedFunction<
+        typeof getAllUsersInMatchmakingQueue
+      >;
+    const mockMatchmaking = matchmaking as jest.MockedFunction<
+      typeof matchmaking
+    >;
+    const mockedCreateLobbyAndInsertPlayersIntoLobby =
+      createLobbyAndInsertPlayersIntoLobby as jest.MockedFunction<
+        typeof createLobbyAndInsertPlayersIntoLobby
+      >;
+    mockedGetAllUsersInMatchmakingQueue.mockResolvedValueOnce(
+      mockGetAllUsers as MatchMakingEntryRowPacket[]
+    );
+    mockMatchmaking.mockReturnValueOnce(mockMatchmakingData);
     await matchmakingJob();
     expect(mockedGetAllUsersInMatchmakingQueue).toHaveBeenCalled();
     expect(mockMatchmaking).toHaveBeenCalled();
